17c9241d73e30d133f0ea915876659360f5855b1
KingSeJong99 <ckrgkstpwhdd@naver.com>
2021-04-02 14:40:50 +0900

    접근 제어자 private와 정보은닉의 개념 이해

---
+++ Chapter5/src/hiding/MyDateTest.java
@@ -0,0 +1,16 @@
+package hiding;
+
+public class MyDateTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MyDate date = new MyDate();
+		
+		date.setYear(2019);
+		date.setMonth(7);
+		date.setDay(100);
+
+		date.showDate();
+	}
+
+}
