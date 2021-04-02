dde5c369d44b8975ce7e51772b835be2b25be5ab
KingSeJong99 <ckrgkstpwhdd@naver.com>
2021-04-02 16:51:38 +0900

    5장 연습문제

---
+++ Practice/src/chapter5/MyDateTest.java
@@ -0,0 +1,13 @@
+package chapter5;
+
+public class MyDateTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MyDate date1 = new MyDate(30, 2, 2000);
+		date1.showDate();
+		MyDate date2 = new MyDate(2, 10, 2006);
+		date2.showDate();
+	}
+
+}
