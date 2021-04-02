5fc7d6da9bf6e2a2a1c537a84516495503b323b9
KingSeJong99 <ckrgkstpwhdd@naver.com>
2021-04-02 14:43:43 +0900

    5장 연습문제

---
+++ Practice/src/chapter5/Order.java
@@ -0,0 +1,12 @@
+package chapter5;
+
+public class Order {
+
+	public long orderId;
+	public String customerId;
+	public String orderDate;
+	public String customerName;
+	public String productId;
+	public String shippingAddress;
+
+}
