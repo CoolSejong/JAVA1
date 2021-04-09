9444930592adbde2bdfa55ff64aaed50e3780133
KingSeJong99 <ckrgkstpwhdd@naver.com>
2021-04-09 20:36:10 +0900

    한 배열의 인덱스 값을 다른 배열의 인덱스에 값 덮어쓰기

--- Chapter6/src/array/ArrayCopy.java
+++ Chapter6/src/array/ArrayCopy.java
@@ -8,6 +8,7 @@ public class ArrayCopy {
 		int[] arr1 = {10, 20, 30, 40, 50};
 		int[] arr2 = {1, 2, 3, 4, 5};
 		
+		// System.arraycopy : 첫 번째 배열의 n 번째 부터 두 번째 배열의 m 부터 k만큼 값을 복사한다. 
 		System.arraycopy(arr1, 0, arr2, 1, 3);
 		
 		for (int i = 0; i < arr2.length; i++)
