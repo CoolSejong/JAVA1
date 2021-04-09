e79a181e64832e228591c3c832f4b1676e4d160c
KingSeJong99 <ckrgkstpwhdd@naver.com>
2021-04-09 20:58:00 +0900

    얕은 복사(shallow copy) 이해

---
+++ Chapter6/src/array/ObjectCopy.java
@@ -0,0 +1,45 @@
+package array;
+
+public class ObjectCopy {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Book[] library = new Book[5];
+		Book[] copyLibrary = new Book[5];
+		
+		library[0] = new Book("태백산맥1", "조정래");
+		library[1] = new Book("태백산맥2", "조정래");
+		library[2] = new Book("태백산맥3", "조정래");
+		library[3] = new Book("태백산맥4", "조정래");
+		library[4] = new Book("태백산맥5", "조정래");
+
+		System.arraycopy(library, 0, copyLibrary, 0, 5);
+		
+		
+		// 자바에서의 또 다른 반복문 선언 방법
+		// 향상된 for문
+		// for('개체의 타입' '변수')
+		for( Book book : copyLibrary) {
+			book.showBookInfo();
+		}
+		
+		System.out.println("================");
+		
+		library[0].setTitle("나목");
+		library[0].setAuthor("박완서");
+		
+		for (Book book : library) {
+			book.showBookInfo();
+		}
+		
+		System.out.println("================");
+		
+		for (Book book : library) {
+			book.showBookInfo();
+		}
+		
+		// 여기까지 한 것을 '얕은 복사'라고 한다. 얕은 복사는 주소값을 복사하는 것을 의미한다.
+	}
+
+}
